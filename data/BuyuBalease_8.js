db63429f23060a4af4a99e9c8dd61dbef4f65452
billymcconvell <43562809+billymcconvell@users.noreply.github.com>
2018-10-16 16:34:22 +0930

    Add files via upload

---
+++ data/BuyuBalease_8.js
@@ -0,0 +1,8 @@
+var json_BuyuBalease_8 = {
+"type": "FeatureCollection",
+"name": "BuyuBalease_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "name": "Buyu Balease", "alt": "3,016m a.s.l." }, "geometry": { "type": "Point", "coordinates": [ 120.542111, -2.406889 ] } }
+]
+}
